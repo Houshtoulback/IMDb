c7a2c6d607452675ad2484ea3c926739db2ac42d
Houshtoul's <hhaghpanah16@gmail.com>
2023-07-04 00:40:37 +0330

    create the about page

---
+++ src/app/about/page.js
@@ -0,0 +1,28 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Magna eu velit adipisicing nisi mollit. Qui aliqua adipisicing mollit
+        fugiat qui eiusmod anim fugiat sint. Commodo tempor nisi cillum ad et et
+        commodo cillum occaecat. Ullamco sint ea do ut eiusmod. Commodo commodo
+        tempor magna ullamco. Qui amet ex exercitation adipisicing eiusmod.
+      </p>
+      <p>
+        Consectetur tempor nisi velit id sunt aute. Eiusmod minim nisi
+        incididunt anim adipisicing sit ad eiusmod cupidatat ea magna. Amet enim
+        magna quis nisi consequat Lorem tempor culpa ea ex quis occaecat.
+        Officia dolor aute adipisicing dolore ea id in quis incididunt sit id
+        qui amet. Nulla sint qui occaecat ipsum laborum laborum voluptate in
+        tempor elit ea ea aliqua irure. Ad occaecat enim nisi sunt amet ea culpa
+        sit minim. Velit dolor consectetur incididunt officia exercitation velit
+        voluptate minim eiusmod. Fugiat cupidatat elit ut consequat quis mollit
+        laboris. Mollit eiusmod aliquip ea est Lorem. Consequat culpa
+        exercitation voluptate ad quis et pariatur do. Ipsum consequat nisi
+        exercitation  id non. Nisi non esse irure elit laborum sint esse
+        esse reprehenderit adipisicing. Non est aliqua voluptate in elit Lorem
+        voluptate.
+        </p>
+    </div>
+  );
+}
